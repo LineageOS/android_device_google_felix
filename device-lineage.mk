5ebca3f24450d35aa7781c76b0dcb36866be8c02
Michael Bestas <mikeioannina@gmail.com>
2024-06-12 19:58:49 +0300

    proprietary-files: AP2A.240605.024
    Change-Id: I1ce822c57a2c0a5ef2a08f211ba6a536334fffed

--- device-lineage.mk
+++ device-lineage.mk
@@ -48,21 +48,23 @@ PRODUCT_PACKAGES += \
 # Codec2
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor \
-    android.hardware.media.c2@1.1.vendor:64 \
     android.hardware.media.c2@1.2.vendor:64 \
     libacryl \
     libacryl_hdr_plugin \
     libavservices_minijail.vendor:64 \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_hidl@1.1.vendor:64 \
     libcodec2_hidl@1.2.vendor:64 \
     libcodec2_soft_common.vendor:64 \
     libcodec2_vndk.vendor \
     libexynosutils \
     libexynosv4l2 \
     libmedia_ecoservice.vendor \
+    libmedia_omx.vendor \
     libsfplugin_ccodec_utils.vendor \
     libstagefright_bufferpool@2.0.1.vendor \
+    libstagefright_omx.vendor:64 \
+    libstagefright_omx_utils.vendor \
+    libstagefright_xmlparser.vendor \
     libvendorgraphicbuffer
 
 # Confirmation UI
@@ -71,6 +73,18 @@ PRODUCT_PACKAGES += \
     android.hardware.confirmationui-lib.trusty:64 \
     android.hardware.confirmationui@1.0.vendor:64 \
     libteeui_hal_support.vendor:64
+
+# Contexthub
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub-V3-ndk.vendor:64 \
+    chre_atoms_log:64 \
+    chre_metrics_reporter:64 \
+    chremetrics-cpp:64
+
+# GPS
+PRODUCT_PACKAGES += \
+    liblzma.vendor:64 \
+    libunwindstack.vendor:64
 
 # Graphics
 PRODUCT_PACKAGES += \
@@ -91,7 +105,8 @@ PRODUCT_PACKAGES += \
 
 # Json
 PRODUCT_PACKAGES += \
-    libjson:64
+    libjson:64 \
+    libjsoncpp.vendor:32
 
 # Nos
 PRODUCT_PACKAGES += \
@@ -113,6 +128,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    libnetutils.vendor \
+    libsqlite.vendor \
+    libziparchive.vendor \
     modem_android_property_manager.vendor:32 \
     modem_android_property_manager_impl.vendor:32 \
     modem_clock_manager.vendor \
@@ -178,6 +196,12 @@ PRODUCT_PACKAGES += \
     android.hardware.weaver-V2-ndk.vendor:64 \
     android.hardware.wifi-V1-ndk.vendor:64 \
     com.google.hardware.pixel.display-V9-ndk.vendor
+
+# Misc
+PRODUCT_PACKAGES += \
+    libbinder.vendor:32 \
+    libevent.vendor \
+    libprocessgroup.vendor:32
 
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
