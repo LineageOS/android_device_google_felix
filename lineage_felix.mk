6eac963d99408c4715aaf7b39afc8cddd6abe025
Michael Bestas <mkbestas@lineageos.org>
2024-08-15 06:31:23 +0300

    Update fingerprint/build description from AP2A.240805.005
    Change-Id: I8a570608fd0db1421c8db34e0e30cadbdb53b856

--- lineage_felix.mk
+++ lineage_felix.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=felix \
-    PRIVATE_BUILD_DESC="felix-user 14 AP2A.240705.004 11875680 release-keys"
+    PRIVATE_BUILD_DESC="felix-user 14 AP2A.240805.005 12025142 release-keys"
 
-BUILD_FINGERPRINT := google/felix/felix:14/AP2A.240705.004/11875680:user/release-keys
+BUILD_FINGERPRINT := google/felix/felix:14/AP2A.240805.005/12025142:user/release-keys
 
 $(call inherit-product, vendor/google/felix/felix-vendor.mk)
