4b90b1a800e226bc38fa17e93183d8941c27129d
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/felix-sepolicy.mk
+++ sepolicy/felix-sepolicy.mk
@@ -4,3 +4,6 @@ BOARD_SEPOLICY_DIRS += device/google/felix/sepolicy/tracking_denials
 
 # Fingerprint
 BOARD_SEPOLICY_DIRS += device/google/felix/sepolicy/fingerprint_capacitance
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
