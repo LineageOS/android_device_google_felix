b4fe89f4bc03cd1e599a24531ed7a3bbe288efa5
Michael Bestas <mkbestas@gmail.com>
2025-06-21 10:45:41 +0300

    felix: Switch to our own kernel
    Change-Id: Iaec4d3704b2b44cd809f24d922f6965312da71e5

--- device-felix.mk
+++ device-felix.mk
@@ -17,9 +17,9 @@
 TARGET_RECOVERY_DEFAULT_ROTATION := ROTATION_RIGHT
 
 TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_FELIX_VERSION)
-# Keeps flexibility for kasan and ufs builds
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_FELIX_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_FELIX_DIR)/kernel-headers
+TARGET_KERNEL_DEVICE := felix
+TARGET_KERNEL_DIR := device/google/$(TARGET_KERNEL_DEVICE)-kernels/$(TARGET_LINUX_KERNEL_VERSION)
+TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 
 $(call inherit-product, device/google/felix/uwb/uwb_calibration_country.mk)
 
@@ -48,14 +48,6 @@ PRODUCT_COPY_FILES += \
 # Recovery files
 PRODUCT_COPY_FILES += \
 	device/google/felix/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.felix.rc
-
-# insmod files. Kernel 5.10 prebuilts don't provide these yet, so provide our
-# own copy if they're not in the prebuilts.
-# TODO(b/369686096): drop this when 5.10 is gone.
-ifeq ($(wildcard $(TARGET_KERNEL_DIR)/init.insmod.*.cfg),)
-PRODUCT_COPY_FILES += \
-	device/google/felix/init.insmod.felix.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.felix.cfg
-endif
 
 # Camera
 PRODUCT_COPY_FILES += \
