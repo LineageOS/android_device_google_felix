9845f42f16bd3c892d642a1cec8b81b42a650239
Michael Bestas <mkbestas@lineageos.org>
2025-08-13 02:22:25 +0000

    Update fingerprint/build description from BP2A.250805.005
    Change-Id: I0a3e1941eaf1fde18e10a30e6edfd484f4e00d5b

--- lineage_felix.mk
+++ lineage_felix.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 2092
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="felix-user 16 BP2A.250705.008 13578956 release-keys" \
-    BuildFingerprint=google/felix/felix:16/BP2A.250705.008/13578956:user/release-keys \
+    BuildDesc="felix-user 16 BP2A.250805.005 13691446 release-keys" \
+    BuildFingerprint=google/felix/felix:16/BP2A.250805.005/13691446:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
