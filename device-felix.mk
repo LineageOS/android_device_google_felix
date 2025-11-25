cbf68655f6a7514f268ae54a985f2b2f156e806a
Michael Bestas <mkbestas@lineageos.org>
2025-11-25 22:54:54 +0200

    felix: Fold lineage makefiles into the main ones
    Change-Id: Id20c69cca379a092453434b2b6848d7f7e0ba3e3

--- device-felix.mk
+++ device-felix.mk
@@ -1,5 +1,7 @@
 #
 # SPDX-FileCopyrightText: 2021 The Android Open-Source Project
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -11,6 +13,7 @@ TARGET_KERNEL_DIR := device/google/$(TARGET_KERNEL_DEVICE)-kernels/$(TARGET_LINU
 TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 
 DEVICE_PACKAGE_OVERLAYS += device/google/felix/felix/overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/felix/overlay-lineage
 
 include device/google/gs201/device-shipping-common.mk
 include device/google/felix/vibrator/cs40l26/device.mk
@@ -270,3 +273,47 @@ PRODUCT_PACKAGES += \
 # Felix: 0x410C
 PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.device_id.product_id=16652
+
+# ANGLE - Almost Native Graphics Layer Engine
+PRODUCT_PACKAGES += \
+    ANGLE \
+    libEGL_angle \
+    libGLESv1_CM_angle \
+    libGLESv2_angle
+
+# Dumpstate
+PRODUCT_PACKAGES += \
+    dump_gsc.sh
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# Fold extensions
+PRODUCT_SYSTEM_SERVER_JARS += \
+    system_ext:felix-services
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-V2-ndk.vendor:64
+
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlayFelix
+
+# Init
+PRODUCT_PACKAGES += \
+    init.recovery.felix.touch.rc
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
+
+# Wireless charging
+include device/google/gs-common/wireless_charger/wireless_charger.mk
--- felix/BoardConfig.mk
+++ felix/BoardConfig.mk
@@ -1,5 +1,7 @@
 #
 # SPDX-FileCopyrightText: 2020 The Android Open-Source Project
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -22,6 +24,10 @@ include device/google/gs201/BoardConfig-common.mk
 include device/google/felix/sepolicy/felix-sepolicy.mk
 include device/google/felix/wifi/BoardConfig-wifi.mk
 
-DEVICE_PATH := device/google/felix
-VENDOR_PATH := vendor/google/felix
-include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigLineage.mk
+# Kernel modules
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_kernel_boot
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_kernel_boot))
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+include $(VENDOR_PATH)/BoardConfigVendor.mk
