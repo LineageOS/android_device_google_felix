bfe6fc382d69f361fda9c76ed73ad5cad03c7b49
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 05:57:43 +0200

    felix: Shuffle BoardConfig
    Change-Id: I920841e4fbd3912cc178912300b81fe86c3147cc

--- felix/BoardConfig.mk
+++ felix/BoardConfig.mk
@@ -5,25 +5,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-TARGET_BOARD_INFO_FILE := device/google/felix/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := felix
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 420
 
+include device/google/gs201/BoardConfig-common.mk
+
+# Kernel
 BOARD_KERNEL_CMDLINE += swiotlb=noforce
 
-# Enable load module in parallel
+# Kernel modules
 BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
 
-# The modules which need to be loaded in sequential
 BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
 BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
 BOARD_KERNEL_CMDLINE += panel-samsung-ana6707-f10.load_sequential=1
 BOARD_KERNEL_CMDLINE += s2mpg12-regulator.load_sequential=1
 
-include device/google/gs201/BoardConfig-common.mk
-include device/google/felix/wifi/BoardConfig-wifi.mk
-
-# Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
@@ -31,8 +29,11 @@ BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/,
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/google/felix/sepolicy/vendor \
+    $(DEVICE_PATH)/sepolicy/vendor \
     hardware/google/pixel-sepolicy/vibrator/common \
     hardware/google/pixel-sepolicy/vibrator/cs40l26
+
+# WiFi
+include $(DEVICE_PATH)/wifi/BoardConfig-wifi.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
