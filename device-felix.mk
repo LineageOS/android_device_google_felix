8c7157be5238fa7c7d8806a639b5aafbcc9e8305
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 07:25:12 +0200

    felix: Shuffle & update product properties
    Change-Id: I3818ee42547db84c7377bb8cbfb9ce1c9674d726

--- device-felix.mk
+++ device-felix.mk
@@ -51,10 +51,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# Bluetooth
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.a2dp_aac.vbr_supported=true
-
 PRODUCT_SOONG_NAMESPACES += \
     device/google/felix
 
@@ -62,19 +58,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# The default value of this variable is false and should only be set to true when
-# the device allows users to enable the seamless transfer feature.
-PRODUCT_PRODUCT_PROPERTIES += \
-   euicc.seamless_transfer_enabled_in_non_qs=true
-
 # SKU specific RROs
 PRODUCT_PACKAGES += \
     SettingsOverlayG0B96 \
     SettingsOverlayG9FPL
-
-# Set support one-handed mode
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode=false
 
 # Hinge angle sensor
 PRODUCT_COPY_FILES += \
@@ -88,68 +75,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Bluetooth OPUS codec
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.opus.enabled=true
-
-# Bluetooth LE Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.leaudio_offload.supported=true \
-    persist.bluetooth.leaudio_offload.disabled=false \
-    ro.bluetooth.leaudio_switcher.supported=true \
-    bluetooth.profile.bap.unicast.client.enabled=true \
-    bluetooth.profile.csip.set_coordinator.enabled=true \
-    bluetooth.profile.hap.client.enabled=true \
-    bluetooth.profile.mcp.server.enabled=true \
-    bluetooth.profile.ccp.server.enabled=true \
-    bluetooth.profile.vcp.controller.enabled=true \
-
-# Override BQR mask to enable LE Audio Choppy report
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.bqr.event_mask=295006 \
-    persist.bluetooth.bqr.vnd_quality_mask=16 \
-    persist.bluetooth.bqr.vnd_trace_mask=0 \
-    persist.bluetooth.vendor.btsnoop=false
-
-# Bluetooth LE Audio CIS handover to SCO
-# Set the property only if the controller doesn't support CIS and SCO
-# simultaneously. More details in b/242908683.
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.leaudio.notify.idle.during.call=true
-
-# BT controller not able to support LE Audio dual mic SWB call
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.leaudio.dual_bidirection_swb.supported=false
-
-# LE Audio Unicast Allowlist
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.leaudio.allow_list=SM-R510,WF-1000XM5,SM-R630
-
-# Disable Bluetooth HAP by default
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.profile.hap.enabled_by_default=false
-
-# Disable Bluetooth LE Audio toggle for ASHA device
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.leaudio.toggle_visible_for_asha=false
-
-# Quick Start device-specific settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.quick_start.oem_id=00e0 \
-    ro.quick_start.device_id=felix
-
-# Set support hide display cutout feature
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_hide_display_cutout=true
-
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
     AvoidAppsInCutoutOverlay
-
-# Bluetooth device id
-# Felix: 0x410C
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.device_id.product_id=16652
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
