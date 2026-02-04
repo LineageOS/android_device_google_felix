fe7ddfa57eb1abe765cd5eef8a6f3db240dd5b0a
Nolen Johnson <johnsonnolen@gmail.com>
2026-02-03 21:34:31 -0500

    Update fingerprint/build description from BP4A.260205.001
    Change-Id: If827d82a1f8240070c28156cff8264302c4cdce6

--- lineage_felix.mk
+++ lineage_felix.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_HEIGHT := 2092
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="felix-user 16 BP4A.260105.004.E1 14587043 release-keys" \
-    BuildFingerprint=google/felix/felix:16/BP4A.260105.004.E1/14587043:user/release-keys \
+    BuildDesc="felix-user 16 BP4A.260205.001 14624666 release-keys" \
+    BuildFingerprint=google/felix/felix:16/BP4A.260205.001/14624666:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
