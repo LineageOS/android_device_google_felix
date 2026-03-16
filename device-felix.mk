00841c5df737e2baa90b4ca3fc6f15a5e0e7ab4c
Michael Bestas <mkbestas@lineageos.org>
2026-03-16 12:21:00 +0200

    felix: Move remaining lineage overlays to gs201
    These are common between all non tablet gs201 devices.
    
    Change-Id: I74ced1edef9086aa2b87a14832a7bd91ca765fbe

--- device-felix.mk
+++ device-felix.mk
@@ -23,9 +23,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.hinge_angle.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hinge_angle.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    device/google/felix/overlay-lineage
-
 PRODUCT_PACKAGES += \
     BiometricGs201Overlay \
     DMServiceOverlayProductGs201 \
